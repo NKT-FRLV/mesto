0d736c6e90cc947cf2204f9d7025fbf41149e12e
FROL <nkt.frlv7@yandex.ru>
2024-04-02 15:01:55 +0200

    cards added, able to delete

--- scripts/index.js
+++ scripts/index.js
@@ -1,9 +1,33 @@
 // @todo: Темплейт карточки
+const cardTemplate = document.querySelector('#card-template').content;
 
 // @todo: DOM узлы
+const cardlist = document.querySelector('.places__list');
 
 // @todo: Функция создания карточки
+function createCard(date, callback) {
+  const cardElement = cardTemplate.querySelector('.card').cloneNode(true);
+  const cardImage = cardElement.querySelector('.card__image');
+  const cardText = cardElement.querySelector('.card__title');
+  cardImage.src = date.link;
+  cardImage.alt = date.name;
+  cardText.textContent = date.name;
+
+  const deleteButton = cardElement.querySelector('.card__delete-button');
+  deleteButton.addEventListener( 'click', () => {
+    callback(cardElement);
+  });
+
+  return cardElement;
+}
 
 // @todo: Функция удаления карточки
+function deleteCard(cardElement) {
+  cardElement.remove();
+}
 
 // @todo: Вывести карточки на страницу
+initialCards.forEach( cardDate => {
+  const cardElement = createCard(cardDate, deleteCard);
+  cardlist.append(cardElement); 
+});
